db74dde0e1eb102db9f5f9ac6e9102bfe354b388
hangtuanthiendl <hangtuanthiendl@gmail.com>
2018-04-24 20:18:10 +0700

    OK
    This reverts commit 5fc53661c13e777422e6a661a920ff7433988284.

--- smc/src/main/java/uit/edu/vn/action/AdminLoginAction.java
+++ smc/src/main/java/uit/edu/vn/action/AdminLoginAction.java
@@ -1,11 +1,5 @@
 package uit.edu.vn.action;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -13,7 +7,6 @@ import org.apache.struts2.ServletActionContext;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-@SuppressWarnings("serial")
 public class AdminLoginAction extends ActionSupport {
 	private String username, password;
 
@@ -32,45 +25,10 @@ public class AdminLoginAction extends ActionSupport {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-		public String conn() {
-			  String ret = ERROR;
-			       Connection conn = null;
 
-			       try {
-			          String URL = "jdbc:mysql://localhost:3306/DOJDB";
-			          Class.forName("com.mysql.jdbc.Driver");
-			          conn = DriverManager.getConnection(URL, "root", "root");
-			          String sql = "SELECT name FROM user WHERE";
-			          sql+=" userName = ? AND password = ?";
-			          PreparedStatement ps = conn.prepareStatement(sql);
-			          ps.setString(1, username);
-			          ps.setString(2, password);
-			          ResultSet rs = ps.executeQuery();
-
-			          while (rs.next()) {
-			           username = rs.getString(1);
-			           ret = SUCCESS;
-			          }
-			       } catch (Exception e) {
-			        addActionError(getText("error.login"));  
-			          ret = ERROR;
-			       } finally {
-			          if (conn != null) {
-			             try {
-			                conn.close();
-			             } catch (Exception e) {
-			             }
-			          }
-			       }
-			       return ret;
-			    }
-	
 	@Override
 	public String execute() {
-	
-		conn();
-		
+
 		HttpServletRequest request = ServletActionContext.getRequest();
 
 		if (this.username.equals("admin@smc.com") && this.password.equals("admin")) {
